bf3e2663124835baa5c582e7136415c55c9fea6f
CampossCaio <campos.f.caio@gmail.com>
2021-04-12 14:48:53 -0300

    Added Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+
+import { SignInButtom } from '../SignInButton';
+import styles from './styles.module.scss';
+
+export function Header() {
+  return(
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="ig.news"/>
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a>Posts</a>
+        </nav>
+        <SignInButtom/>
+      </div>
+    </header>
+  )
+}
