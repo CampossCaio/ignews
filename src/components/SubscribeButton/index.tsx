542351b37403cf63f32b1c45235e5fa82db56d07
Caio Figueiredo Campos <caio.campos@hotbhza004537.local>
2021-04-26 22:12:46 -0300

    Create subscription feature

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,12 +1,53 @@
+import { Session } from "next-auth/adapters";
+import { useSession, signIn  } from "next-auth/client";
+import { useRouter } from "next/router";
+import { api } from "../../services/api";
+import { getStripeJs  } from '../../services/stripe-js';
+
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 
+
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+
+  const [ session ] = useSession();
+
+  const { push } = useRouter();
+
+  async function handleSubscribe() {
+    if(!session) {
+      signIn();
+      return;
+    }
+
+    if(session.activeSubscription) {
+      return push('/posts');
+    }
+
+    // criacao da checkout session
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+
+    }catch(err) {
+      alert(err.message);
+    }
+  }
+  
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button 
+      type="button" 
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
